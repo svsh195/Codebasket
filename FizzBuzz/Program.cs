a815e021883c33f52088335d048eeefbe1ba76ba
svsh195 <svsh195@gmail.com>
2022-11-01 18:43:32 +0700

    FizzBuzz

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿for (int i = 1; i <= 100; i++)
+{
+    if (i % 3 == 0)
+    {
+        if (i % 5 == 0)
+        {
+            Console.WriteLine("FizzBuzz");
+        }
+        else
+        {
+            Console.WriteLine("Fizz");
+        }
+    }
+    else if (i % 5 == 0)
+    {
+        Console.WriteLine("Buzz");
+    }
+    else
+    {
+        Console.WriteLine(i);
+    }
+}    
